8eef86adef4ba9152c8421026a39fa78a6d8cd2b
Gabriel Nunes <leirbagmelo2@gmail.com>
2023-04-22 17:44:34 -0300

    Começando a implementar o tabuleiro

---
+++ ConsoleApp1/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,17 @@
+﻿namespace Tabuleiro
+{
+    public class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+
+        private Peca[,] Peca { get; set; }
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Peca = new Peca[linhas, colunas];
+        }
+    }
+}
