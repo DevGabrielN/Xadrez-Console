3adb56d31ad40dd82b8e53a82d17f397f9b2186e
Gabriel Nunes <leirbagmelo2@gmail.com>
2023-04-28 22:44:24 -0300

    Criando a classe PosicaoXadrez

--- ConsoleApp1/Tabuleiro/Tabuleiro.cs
+++ ConsoleApp1/Tabuleiro/Tabuleiro.cs
@@ -13,14 +13,41 @@
             Colunas = colunas;
             Peca = new Peca[linhas, colunas];
         }
+        public Peca Pecas(Posicao pos)
+        {
+            return Peca[pos.Linha, pos.Coluna];
+        }
         public Peca Pecas(int linha, int coluna)
         {
             return Peca[linha, coluna];
         }
+        public bool ExistePeca(Posicao pos) {
+            ValidarPosicao(pos);
+            return Pecas(pos) != null;
+        }
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             Peca[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+        public bool PosicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha >=Linhas || pos.Coluna<0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidarPosicao(Posicao pos)
+        {
+            if(!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida");
+            }
         }
     }
 }
